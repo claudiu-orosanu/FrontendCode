60c960c16f47ccfcbba8a971bfa684c17aa5bafb
claudiu-orosanu <orosanu.claudiu@gmail.com>
2016-06-24 12:56:00 +0300

    +Exercitii suplimentare(partial)

--- Day 1/first/scripts/app.js
+++ Day 1/first/scripts/app.js
@@ -5,31 +5,31 @@ var employeesList = [
     {
         firstName: 'John',
         lastName: 'King',
-        phone: '0123456789',
+        phone: '1283482739',
         salary: 4500
     },
     {
         firstName: 'Steven',
         lastName: 'Gerard',
-        phone: '0123456789',
+        phone: '0273018293',
         salary: 4500
     },
     {
         firstName: 'Diana',
         lastName: 'Ross',
-        phone: '0123456789',
+        phone: '3840182083',
         salary: 4500
     },
     {
         firstName: 'Mike',
         lastName: 'Bob',
-        phone: '0123456789',
+        phone: '3748722125',
         salary: 4500
     },
     {
         firstName: 'Emily',
         lastName: 'Hudson',
-        phone: '0123456789',
+        phone: '1927823921',
         salary: 4500
     }
 ];
@@ -38,8 +38,65 @@ var employeesList = [
         var myTable = '<table border="1" class="table table-condensed"><tr><th>First Name</th><th>LastName</th><th>Phone</th><th>Salary</th></tr>';
 
         for (var i in employeesList) {
-            myTable += '<tr><td>' + employeesList[i].firstName + '</td><td>' + employeesList[i].lastName + '</td><td>' + employeesList[i].phone + '</td><td>' + employeesList[i].salary + '</td></tr>';
+            myTable += '<tr><td>' + employeesList[i].firstName + '</td><td>' + employeesList[i].lastName + '</td><td>' + employeesList[i].phone +
+                '</td><td>' + employeesList[i].salary + '</td> +' +
+                '<td> <button id= "vizButton" onclick="showItem()">Viz</button></td>' +
+                '<td><button id="delButton" onclick="deleteItem()">Remove</button> </td>'+
+                '</tr>';
         }
+
+        //---------------coloana a 2 a, afiseaza numarul de lastname unice------------------
+        var lastNameList = new Array();
+        for (var i in employeesList){
+            lastNameList.push(employeesList[i].lastName);
+        }
+        var countLastName = 0;
+        for(var i in lastNameList){
+            var unique = true;
+            for(var j in lastNameList){
+                if (lastNameList[i] == lastNameList[j] && i!=j)
+                    unique = false;
+            }
+            if(unique == true)
+                countLastName++;
+        }
+        //---------------------------------------------------------
+
+        //-------------coloana a 3 a, sa se afiseze primele 5 cifre care apar cel mai des-------------
+        var phoneList = new Array();
+        for(var i in employeesList)
+            phoneList.push(employeesList[i].phone);
+        var phoneFreq = new Array(10);
+        phoneFreq.fill(0);
+        for(var i in phoneList){
+            for(var j = 0; j<phoneList[i].length; j++){
+                var cif = (phoneList[i])[j];
+                phoneFreq[cif] ++;
+            }
+        }
+        //alert('phoneFreq =' + phoneFreq);
+        var maxPhone = new Array();
+        for(var k=0; k<5; k++) {
+            var max = phoneFreq[0];
+            var index =0;
+            for (var i in phoneFreq) {
+                if (phoneFreq[i] > max) {
+                    max = phoneFreq[i];
+                    index = i;
+                }
+            }
+            phoneFreq[index] = 0;
+            maxPhone.push(index);
+        }
+        //alert('maxPhone =' + maxPhone);
+        //---------------------------------------------------------
+
+
+        myTable += '<tr><td>s</td><td>' +countLastName +'</td><td>'+
+            maxPhone[0]+','+maxPhone[1]+','+
+            maxPhone[2]+ ',' + maxPhone[3]+ ',' +maxPhone[4]+
+            '</td></tr>';
+
         myTable += '</table>';
         var container = document.getElementById('listcontainer');
         container.innerHTML = myTable;
@@ -55,7 +112,7 @@ var employeesList = [
         var firstName = document.getElementById("firstName").value;
         var lastName = document.getElementById("lastName").value;
         var phone = document.getElementById("phone").value;
-        var salary = document.getElementById("salary").value;
+        var salary = parseInt(document.getElementById("salary").value);
         employeesList.push(new Employee(firstName, lastName, phone, salary));
         showList();
     }
@@ -71,4 +128,7 @@ var employeesList = [
     function deleteLast() {
         employeesList.pop();
         showList();
+    }
+
+    function showItem(){
     }
